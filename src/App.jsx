1328d40341c75e81f5b417b1791cee151c4b2f05
remmouche <121981104+remmouche@users.noreply.github.com>
2024-11-22 16:20:03 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      App
+    </div>
+  );
+}
+
+export default App;
