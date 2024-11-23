f31804793fc36980ff5a0ab1b120df1e0bee6b98
Ahsene Remmouche <ahsene.remmouche@outlook.com>
2024-11-23 04:00:05 +0000

    add css

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import logo from "../assets/reactRouter.svg"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <img src={logo} alt="" />
+      <ul>
+        <li>Home</li>
+        <li>Products</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+
+      <button>Get Started</button>
+      
+      </div>
   )
 }
 
