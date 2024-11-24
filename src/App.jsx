41cfad51a54d10d643800eb2cdd1b4da33062ed4
Ahsene Remmouche <ahsene.remmouche@outlook.com>
2024-11-24 04:17:26 +0000

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Navbar />
-      <div className="container">
-      <h1>App</h1>
-      </div>
-      
+      <Router>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<h1>Home</h1>} />
+            <Route path="/products" element={<h1>Products</h1>} />
+            <Route path="/about" element={<h1>About</h1>} />
+            <Route path="/contact" element={<h1>Contact</h1>} />
+
+          </Routes>
+        </div>
+      </Router>
+
     </div>
   );
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React  from 'react'
 import logo from "../assets/reactRouter.svg"
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
+  
   return (
     <div className='navbar'>
       <img src={logo} alt="" />
       <ul>
-        <li>Home</li>
-        <li>Products</li>
-        <li>About</li>
-        <li>Contact</li>
+        <NavLink to="/"><li>Home</li></NavLink>
+        <NavLink to="/products"><li>Products</li></NavLink>
+        <NavLink to="/about"><li>About</li></NavLink>
+        <NavLink to="/contact"><li>Contact</li></NavLink>
+        {/* <NavLink to="/contact"> <li>Contact</li></NavLink> */}
+       
       </ul>
 
       <button>Get Started</button>
